0985d6c115d48f02f8da5d811bb9fd6d0ee09ab2
David Anderson <david.anderson.sil@gmail.com>
2019-09-22 21:33:03 -0300

    chore: Add commands required to run application using Docker

--- run_docker.sh
+++ run_docker.sh
@@ -3,4 +3,12 @@
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --force-rm --no-cache
 
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --force-recreate --remove-orphans -V
+
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml exec php composer install -o
+
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml exec php bin/console cache:clear
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml exec php bin/console cache:warmup
+
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml exec php bin/console doctrine:schema:create
+
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml exec php bash
